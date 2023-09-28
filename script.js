2767e4e4f9bd0db7e1a404ae16bde72482a8b339
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-09-28 09:59:59 -0500

    create a checkbox for marking tasks as completed

--- script.js
+++ script.js
@@ -12,5 +12,10 @@ function createTaskElement(taskText) {
 
   const taskDisplay = document.createElement('span');
   taskDisplay.textContent = taskText;
+
+  // create a checkbox for marking tasks as completed
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.classList.add('task-checkbox');
   
 }// end createTaskElement
