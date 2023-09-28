b887c91ab291764540819e40d14cd4067329156e
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-09-28 09:32:54 -0500

    get references to HTML elements taskinput, addtask, tasklist

---
+++ script.js
@@ -0,0 +1,4 @@
+// Get references to HTML elements
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
