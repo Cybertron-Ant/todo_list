4ac9f1baa17d538be5e800e5f5872a40fc86a97a
Antonio Fuller <Mediainformationofficer@gmail.com>
2023-09-28 09:57:39 -0500

    Update script.js, function to create a new task element

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 const taskInput = document.getElementById('taskInput');
 const addTaskButton = document.getElementById('addTask');
 const taskList = document.getElementById('taskList');
+
+// function to create a new task element
+function createTaskElement(taskText) {
+  
+}// end createTaskElement
